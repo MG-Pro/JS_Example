22990a6a708fcea0cee420f0b6e0df4f36fbaf45
MG <mg84@bk.ru>
2019-10-16 11:55:38 +0300

    add increment

---
+++ scripts/increment.js
@@ -0,0 +1,18 @@
+class Increment {
+    constructor (i = 0) {
+        this.i = i;
+    }
+    toString() {
+        console.log('toString');
+        return ++this.i;
+    }
+}
+
+const increment = new Increment(0);
+
+alert(increment); // 1
+alert(increment); // 2
+
+alert(increment + increment); // 7
+
+
